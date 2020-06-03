aff34843549984f62243df2128f8b391df09ef82
Russell Gordon <rgordon@lcs.on.ca>
2020-06-03 15:59:15 -0400

    Initial Commit

---
+++ BarcodeScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeScanner
+//
+//  Created by Russell Gordon on 2020-06-03.
+//  Copyright © 2020 Russell Gordon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
